edd6fbeba81e70b243c106ae90fcd3c509be8842
GabrielVasconcelo5 <gabrvasco2@gmail,com>
2023-05-18 08:27:23 -0300

    Adão é um power ranger

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        // Aula de Orientacoes D Objetos
+        // Declarar objetos
+        Pessoa adao;
+        // Instanciar objetos
+        adao = new Pessoa();
+        Pessoa eva = new Pessoa();
+        // Definicao do formyo
+        adao.nome = "Adão";
+        adao.sobrenome = "Silva";
+        eva.nome = "Eva";
+        eva.sobrenome = "Silva";
+        // Acionar comportamento
+        adao.falar();
+        eva.falar();
+
         
     }
 }
---
+++ src/Pessoa.java
@@ -0,0 +1,9 @@
+public class Pessoa {
+    // Atributos da classe
+    String nome;
+    String sobrenome;
+    // Metodo da classe
+    public void falar(){
+        System.out.println("falei");
+    }
+}
